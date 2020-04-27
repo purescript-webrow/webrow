9323527b7241832bf02d2765632183af91b59a2e
Kamirus <listopadkamil@gmail.com>
2020-04-27 11:20:07 +0200

    test server: prepared for payment applet

--- src/Main.purs
+++ src/Main.purs
@@ -3,36 +3,60 @@ module Main where
 import Prelude
 
 import Data.Either (either)
+import Data.Generic.Rep (class Generic)
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class.Console (log)
 import HTTPure as HTTPure
 import Routing.Duplex as D
+import Routing.Duplex.Generic as DG
 import Run (AFF, Run, runBaseAff)
 import Run as Run
-import Run.Reader (runReader)
+import Run.Reader (READER, runReader)
 import ShopUtils.Crypto (Secret(..))
-import ShopUtils.Logging.Effect (runLoggerConsole)
+import ShopUtils.Logging.Effect (LOGGER, runLoggerConsole)
 import ShopUtils.Mailer (MailerF(..), MAILER, _mailer)
 import ShopUtils.Register as Register
+
+data Route
+  = RegisterRoute Register.Route
+  -- | PaymentRoute Payment.Route
+derive instance genericRoute ∷ Generic Route _
+
+route ∷ D.RouteDuplex' Route
+route = D.root $ DG.sum
+  { "RegisterRoute": Register.route
+  -- , "PaymentRoute": Payment.route
+  }
 
 router ∷ HTTPure.Request → Aff HTTPure.Response
 router req = do
   let url = HTTPure.fullPath req
   -- log url
-  D.parse (D.root Register.route) url
-    # either (HTTPure.badRequest <<< show) \route → do
-      Register.onRegisterRoute route
-        # Register.runRegister
-        # interpretMailerStub
-        # runReader { secret: Secret "g6f7s8r328h0ej906291d" }
-        # runLoggerConsole
-        # runBaseAff
+  D.parse route url # either (HTTPure.badRequest <<< show)
+    (interpretBaseEffects <<< onRootRoute)
+
+onRootRoute ∷ Route → BaseRun HTTPure.Response
+onRootRoute = case _ of
+  RegisterRoute rr → Register.onRegisterRoute rr # Register.runRegister
 
 main ∷ Effect Unit
 main = do
   -- log $ D.print Register.route $ Register.RegisterEmail $ Email "someemail@gmail.com"
   void $ HTTPure.serve 8080 router $ log "Server now up on port 8080"
+
+type BaseRun = Run
+  ( aff ∷ AFF
+  , logger ∷ LOGGER
+  , mailer ∷ MAILER
+  , reader ∷ READER { secret ∷ Secret }
+  )
+
+interpretBaseEffects ∷ BaseRun ~> Aff
+interpretBaseEffects = interpretMailerStub
+  >>> runReader { secret: Secret "g6f7s8r328h0ej906291d" }
+  >>> runLoggerConsole
+  >>> runBaseAff
 
 interpretMailerStub
   ∷ ∀ a eff
